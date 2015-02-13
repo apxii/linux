1138195b7d449fc2c11a19c3b7db9a86b1aad32e
Russell King <rmk+kernel@arm.linux.org.uk>
2015-02-13 11:32:03 +0100

    staging: etnaviv: move PM calls into bind/unbind callbacks
    Power management of each GPU core should be a matter for the driver of
    each GPU core itself.  Move the basic power management calls out of
    etnaviv_drv.c and into etnaviv_gpu.c so that we can add runtime PM.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/staging/etnaviv/etnaviv_gpu.c
+++ drivers/staging/etnaviv/etnaviv_gpu.c
@@ -643,40 +643,6 @@ static int disable_axi(struct etnaviv_gpu *gpu)
 	return 0;
 }
 
-int etnaviv_gpu_pm_resume(struct etnaviv_gpu *gpu)
-{
-	int ret;
-
-	DBG("%s", dev_name(gpu->dev));
-
-	ret = enable_clk(gpu);
-	if (ret)
-		return ret;
-
-	ret = enable_axi(gpu);
-	if (ret)
-		return ret;
-
-	return 0;
-}
-
-int etnaviv_gpu_pm_suspend(struct etnaviv_gpu *gpu)
-{
-	int ret;
-
-	DBG("%s", dev_name(gpu->dev));
-
-	ret = disable_axi(gpu);
-	if (ret)
-		return ret;
-
-	ret = disable_clk(gpu);
-	if (ret)
-		return ret;
-
-	return 0;
-}
-
 /*
  * Hangcheck detection for locked gpu:
  */
@@ -946,6 +912,38 @@ static irqreturn_t irq_handler(int irq, void *data)
 	return ret;
 }
 
+static int etnaviv_gpu_resume(struct etnaviv_gpu *gpu)
+{
+	int ret;
+
+	ret = enable_clk(gpu);
+	if (ret)
+		return ret;
+
+	ret = enable_axi(gpu);
+	if (ret) {
+		disable_clk(gpu);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int etnaviv_gpu_suspend(struct etnaviv_gpu *gpu)
+{
+	int ret;
+
+	ret = disable_axi(gpu);
+	if (ret)
+		return ret;
+
+	ret = disable_clk(gpu);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
 static int etnaviv_gpu_bind(struct device *dev, struct device *master,
 	void *data)
 {
@@ -953,6 +951,7 @@ static int etnaviv_gpu_bind(struct device *dev, struct device *master,
 	struct etnaviv_drm_private *priv = drm->dev_private;
 	struct etnaviv_gpu *gpu = dev_get_drvdata(dev);
 	int idx = gpu->pipe;
+	int ret;
 
 	dev_info(dev, "pre gpu[idx]: %p\n", priv->gpu[idx]);
 
@@ -965,6 +964,10 @@ static int etnaviv_gpu_bind(struct device *dev, struct device *master,
 	}
 
 	dev_info(dev, "post gpu[idx]: %p\n", priv->gpu[idx]);
+
+	ret = etnaviv_gpu_resume(gpu);
+	if (ret < 0)
+		return ret;
 
 	gpu->drm = drm;
 
@@ -989,6 +992,8 @@ static void etnaviv_gpu_unbind(struct device *dev, struct device *master,
 	hangcheck_disable(gpu);
 
 	WARN_ON(!list_empty(&gpu->active_list));
+
+	etnaviv_gpu_suspend(gpu);
 
 	if (gpu->buffer) {
 		drm_gem_object_unreference(gpu->buffer);
