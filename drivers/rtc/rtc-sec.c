8ac01b218920d73828b848316a1d8489682b2edf
Andrey Panov <rockford@yandex.ru>
2015-05-03 19:24:23 +0300

    Odroid XU3: Disable debug messages in Samsung RTC driver
    Signed-off-by: Andrey Panov <rockford@yandex.ru>

--- drivers/rtc/rtc-sec.c
+++ drivers/rtc/rtc-sec.c
@@ -150,10 +150,6 @@ static int s2m_rtc_read_time(struct device *dev, struct rtc_time *tm)
 
 	s2m_data_to_tm(data, tm, info->rtc_24hr_mode);
 
-	printk(KERN_INFO "%s: %d/%d/%d %d:%d:%d(%d)\n", __func__,
-		1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday,
-		tm->tm_hour, tm->tm_min, tm->tm_sec, tm->tm_wday);
-
 	return rtc_valid_tm(tm);
 }
 
@@ -171,10 +167,6 @@ static int s2m_rtc_set_time(struct device *dev, struct rtc_time *tm)
 
 	ret = s2m_rtc_set_time_reg(info, 0);
 
-	printk(KERN_INFO "%s: %d/%d/%d %d:%d:%d(%d)\n", __func__,
-		1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday,
-		tm->tm_hour, tm->tm_min, tm->tm_sec, tm->tm_wday);
-
 	return ret;
 }
 
@@ -190,12 +182,6 @@ static int s2m_rtc_read_alarm(struct device *dev, struct rtc_wkalrm *alrm)
 		return ret;
 
 	s2m_data_to_tm(data, &alrm->time, info->rtc_24hr_mode);
-
-	printk(KERN_INFO "%s: %d/%d/%d %d:%d:%d(%d)\n", __func__,
-		1900 + alrm->time.tm_year, 1 + alrm->time.tm_mon,
-		alrm->time.tm_mday, alrm->time.tm_hour,
-		alrm->time.tm_min, alrm->time.tm_sec,
-		alrm->time.tm_wday);
 
 	alrm->enabled = 0;
 
@@ -275,10 +261,6 @@ static int s2m_rtc_stop_alarm(struct s2m_rtc_info *info)
 
 	s2m_data_to_tm(data, &tm, info->rtc_24hr_mode);
 
-	printk(KERN_INFO "%s: %d/%d/%d %d:%d:%d(%d)\n", __func__,
-		1900 + tm.tm_year, 1 + tm.tm_mon, tm.tm_mday,
-		tm.tm_hour, tm.tm_min, tm.tm_sec, tm.tm_wday);
-
 	for (i = 0; i < 7; i++)
 		data[i] &= ~ALARM_ENABLE_MASK;
 
@@ -301,10 +283,6 @@ static int s2m_rtc_start_alarm(struct s2m_rtc_info *info)
 		return ret;
 
 	s2m_data_to_tm(data, &tm, info->rtc_24hr_mode);
-
-	printk(KERN_INFO "%s: %d/%d/%d %d:%d:%d(%d)\n", __func__,
-		1900 + tm.tm_year, 1 + tm.tm_mon, tm.tm_mday,
-		tm.tm_hour, tm.tm_min, tm.tm_sec, tm.tm_wday);
 
 	data[RTC_SEC] |= ALARM_ENABLE_MASK;
 	data[RTC_MIN] |= ALARM_ENABLE_MASK;
@@ -333,11 +311,6 @@ static int s2m_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alrm)
 	int ret;
 
 	s2m_tm_to_data(&alrm->time, data);
-
-	printk(KERN_INFO "%s: %d/%d/%d %d:%d:%d(%d)\n", __func__,
-		1900 + alrm->time.tm_year, 1 + alrm->time.tm_mon,
-		alrm->time.tm_mday, alrm->time.tm_hour, alrm->time.tm_min,
-		alrm->time.tm_sec, alrm->time.tm_wday);
 
 	ret = s2m_rtc_stop_alarm(info);
 	if (ret < 0)
